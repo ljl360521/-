9902df545af598da195151621d9ab8c1ff71005a
ljl360521 <ljl360521@users.noreply.github.com>
2026-06-25 17:44:40 +0000

    删除 unity通用注入模板 项目，准备开始新项目

--- unity通用注入模板/modules/native/include/dex_loader.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <jni.h>
-#include <string>
-
-jobject     createDexByteBuffer();
-jclass      loadImGuiClassFromDex();
-bool        callImGuiSetupView(jclass imguiClass);
--- unity通用注入模板/modules/unitymatrix/include/Unity.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "Vector2.hpp"
-#include "Vector3.hpp"
-#include "Vector4.hpp"
-#include "Quaternion.hpp"
-#include "Matrix3x3.hpp"
-#include "Matrix4x4.hpp"
